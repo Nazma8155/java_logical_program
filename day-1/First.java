d5857b9ab910d069315cefc2cd57cfb772bf3d41
Nazma8155 <sknazma8166@gmail.com>
2024-11-07 14:23:18 +0530

    Add files via upload

---
+++ day-1/First.java
@@ -0,0 +1,12 @@
+class First1
+{
+public static void main(String arg[])
+{
+int empno=111
+String empname="nazma";
+float empsal=25000F;
+System.out.println("the given employee number is:"+empno);
+System.out.println("the given employee name is:"+empname);
+System.out.println("the given employee salary is:"+empsal);
+}
+}
